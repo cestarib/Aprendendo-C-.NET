7f70cc807e8fce0d51e9009b3f62fa54d5ddca0a
cestarib <brunovrs@gmail.com>
2024-11-26 16:22:16 -0300

    webapi

---
+++ Web Api/WebApi8/Models/LivroModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi8.Models
+{
+    public class LivroModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        public ICollection<AutorModel> Autor { get; set; }
+
+    }
+}
